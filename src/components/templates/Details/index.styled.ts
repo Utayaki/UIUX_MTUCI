7facd095189946bee1d1871f58308be0a135e74a
Utayaki <helloyoutube31@yandex.ru>
2023-12-08 17:30:23 +0000

    [new] Added comments

--- src/components/templates/Details/index.styled.ts
+++ src/components/templates/Details/index.styled.ts
@@ -1,231 +1,304 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Details = styled.section`
-    display: flex; 
-    width: 100%;
-    height: 100%;
-    flex: 0 0 auto;
-    justify-content: center;
-    background-color: ${props => props.theme.bachgroundColor};
-    transition: 0.1s;
-    position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex: 0 0 auto;
+  justify-content: center;
+  background-color: ${(props) => props.theme.bachgroundColor};
+  transition: 0.1s;
+  position: relative;
 `;
 
 export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 1000px;
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  padding: 0px 20px;
+  margin: 0px 0px;
+  box-sizing: border-box;
+  z-index: 1;
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  @media (max-width: 1200px) and (min-width: 992px) {
+    width: 100%;
+  }
+  @media (max-width: 992px) and (min-width: 768px) {
     padding: 0px 20px;
     margin: 0px 0px;
-    box-sizing: border-box;
-    z-index: 1;
-    @media (min-width: 1200px){
-        width: 1200px;
-    }
-    @media (max-width: 1200px) and (min-width: 992px)  {
-        width: 100%;
-    }
-    @media (max-width: 992px) and (min-width: 768px)  {
-        padding: 0px 20px;   
-        margin: 0px 0px;
-        width: 100%;
-    }
-    @media (max-width: 768px) and (min-width: 576px)  { 
-        padding: 0px 20px;   
-        margin: 0px 0px;
-        width: 100%;
-    }
-    @media (max-width: 576px)  {
-        padding: 0px 20px;
-        margin: 0px 0px;
-        width: 100%;
-    }
+    width: 100%;
+  }
+  @media (max-width: 768px) and (min-width: 576px) {
+    padding: 0px 20px;
+    margin: 0px 0px;
+    width: 100%;
+  }
+  @media (max-width: 576px) {
+    padding: 0px 20px;
+    margin: 0px 0px;
+    width: 100%;
+  }
 `;
 export const ContentTitle = styled.span`
-    letter-spacing: 1px;
+  letter-spacing: 1px;
+  font-weight: 500;
+  display: flex;
+  flex: 0 0 auto;
+  width: 100%;
+  margin: 200px 0px;
+  align-items: center;
+  box-sizing: border-box;
+  font-size: ${(props) => props.theme.textSizeTitle};
+  @media (max-width: 768px) and (min-width: 576px) {
+    font-size: ${(props) => props.theme.textSizeTitle700};
+  }
+  @media (max-width: 576px) {
+    font-size: ${(props) => props.theme.textSizeTitle500};
+  }
+  & a {
+    font-family: Roboto;
+    font-size: ${(props) => props.theme.textSizeTitle};
     font-weight: 500;
-    display: flex;
-    flex: 0 0 auto;
-    width: 100%;
-    margin: 50px 0px;
-    align-items: center;
-    box-sizing: border-box;
-    font-size: ${props => props.theme.textSizeTitle};
-    @media (max-width: 768px) and (min-width: 576px)  {
-        font-size: ${props => props.theme.textSizeTitle700};
+    letter-spacing: 1px;
+    transition: 0.3s;
+    &:hover {
+      transition: 0.3s;
+      color: ${(props) => props.theme.accentColor1};
     }
-    @media (max-width: 576px)  {
-        font-size: ${props => props.theme.textSizeTitle500};
-    }
-    & a{
-        font-family: Roboto;
-        font-size:  ${props => props.theme.textSizeTitle};
-        font-weight: 500;
-        letter-spacing: 1px;
-        transition: 0.3s;
-        &:hover{
-            transition: 0.3s;
-            color: ${props => props.theme.accentColor1};
-        }
-    }
+  }
 `;
 
 export const Data = styled.div`
-    display: grid;
-    width: 100%;
-    align-items: center;
-    align-self: center;
-    grid-template-columns: 1fr 2fr ;
-    grid-gap: 20px;
-    border-top: 1px solid ${props => props.theme.accentColor1};
-    border-bottom: 1px solid ${props => props.theme.accentColor1};
-    padding: 20px 0px;
-    box-sizing: border-box; 
-    @media (min-width: 1200px){   
-    }
-    @media (max-width: 1200px) and (min-width: 992px)  {     
-    }
-    @media (max-width: 992px) and (min-width: 768px)  {     
-    }
-    @media (max-width: 768px) and (min-width: 576px)  {    
-    }
-    @media (max-width: 576px)  {
-        grid-template-columns: 1fr;
-    }
+  display: grid;
+  width: 100%;
+  align-items: center;
+  align-self: center;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 20px;
+  border-top: 1px solid ${(props) => props.theme.accentColor1};
+  border-bottom: 1px solid ${(props) => props.theme.accentColor1};
+  padding: 20px 0px;
+  box-sizing: border-box;
+  @media (min-width: 1200px) {
+  }
+  @media (max-width: 1200px) and (min-width: 992px) {
+  }
+  @media (max-width: 992px) and (min-width: 768px) {
+  }
+  @media (max-width: 768px) and (min-width: 576px) {
+  }
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const BackgroundImage = styled.img`
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 600px;
-    object-fit: cover;
-    opacity: 0.3;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 600px;
+  object-fit: cover;
+  opacity: 0.3;
 `;
 
 export const Image = styled.div`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
 `;
 export const Img = styled.img`
-    aspect-ratio: 1/1/5;
-    display: flex;
-    width: 100%;
-    object-fit: cover;
-    box-sizing: border-box;
-    transition: 0.5s;
-    box-shadow: 0px 3px 5px ${props=>props.theme.shadowColor};;
-    margin: 0px 0px 20px 0px;
+  aspect-ratio: 1/1/5;
+  display: flex;
+  width: 100%;
+  object-fit: cover;
+  box-sizing: border-box;
+  transition: 0.5s;
+  box-shadow: 0px 3px 5px ${(props) => props.theme.shadowColor};
+  margin: 0px 0px 20px 0px;
 `;
 
 export const Buttons = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 20px;
-    @media (max-width: 768px) and (min-width: 576px)  {
-        grid-template-columns: 1fr  ;
-    }
-    @media (max-width: 576px)  {
-        grid-template-columns: 1fr  ;
-    }
-
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  @media (max-width: 768px) and (min-width: 576px) {
+    grid-template-columns: 1fr;
+  }
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Button = styled.a`
-    cursor: pointer;
-    display: flex;
-    width: 100%;
-    height: 40px;
-    align-items: center;
-    justify-content: center;
-    color:white;
-    background-color: aqua;
-    border: 0px;
-    border-radius:10px;
-    font-size: ${props=>props.theme.textSizeTextM};
-    &:hover{
-        opacity: 0.9;
-    }
+  cursor: pointer;
+  display: flex;
+  width: 100%;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  background-color: aqua;
+  border: 0px;
+  border-radius: 10px;
+  font-size: ${(props) => props.theme.textSizeTextM};
+  &:hover {
+    opacity: 0.9;
+  }
 `;
 export const DownloadButton = styled(Button)`
-    background-color: ${props=>props.theme.accentColor1};
+  background-color: ${(props) => props.theme.accentColor1};
 `;
 export const WatchButton = styled(Button)`
-    background-color:${props=>props.theme.accentColor2};
+  background-color: ${(props) => props.theme.accentColor2};
 `;
 
 export const Description = styled.div`
-    display: grid;
-    align-items: flex-start;
-    align-self: flex-start;
-    grid-template-columns: 1fr;
-    grid-gap: 20px;
+  display: grid;
+  align-self: flex-start;
+  align-items: center;
+  justify-content: bottom;
+  grid-template-columns: 1fr;
+  grid-gap: 20px;
 `;
 
 export const Title = styled.div`
-    display: flex;
-    font-size: ${props=> props.theme.textSizeTitle};
-    letter-spacing: 1px;
-    font-weight: 500;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${(props) => props.theme.textSizeTitle};
+  letter-spacing: 1px;
+  font-weight: 500;
 `;
 
 export const Year = styled.div`
-    display: flex;
-    width: 100%;
-    flex: 0 0 auto;
-    font-weight: 600;
-    font-size: ${props=> props.theme.textSizeTextL};
-    color: ${props=> props.theme.textColor};
-
-
+  display: flex;
+  width: 100%;
+  flex: 0 0 auto;
+  font-weight: 600;
+  font-size: ${(props) => props.theme.textSizeTextL};
+  color: ${(props) => props.theme.textColor};
 `;
 export const DescriptionFull = styled.div`
-    display: flex;
-    font-size: ${props=> props.theme.textSizeTextM};
-    line-height: 25px;
-    letter-spacing: 1px;
-
+  display: flex;
+  font-size: ${(props) => props.theme.textSizeTextM};
+  line-height: 25px;
+  letter-spacing: 1px;
 `;
 export const Genres = styled.div`
-    display: grid;
-    font-size: ${props=> props.theme.textSizeTextM};
-    grid-template-columns: repeat(5,1fr);
-    grid-gap:10px;
-    @media (max-width: 768px) and (min-width: 576px)  {
-        grid-template-columns: 1fr  ;
-    }
-    @media (max-width: 576px)  {
-        grid-template-columns: 1fr  ;
-    }
+  display: grid;
+  font-size: ${(props) => props.theme.textSizeTextM};
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 10px;
+  @media (max-width: 768px) and (min-width: 576px) {
+    grid-template-columns: 1fr;
+  }
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const Statistic = styled.div`
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    grid-gap:20px;
-    @media (max-width: 768px) and (min-width: 576px)  {
-        grid-template-columns: 1fr 1fr  1fr 1fr;
-    }
-    @media (max-width: 576px)  {
-        grid-template-columns: 1fr 1fr  ;
-    }
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 20px;
+  @media (max-width: 768px) and (min-width: 576px) {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
+  @media (max-width: 576px) {
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 export const TorrentsTitle = styled.div`
-    display: flex;
-    width: 100%;
-    flex: 0 0 auto;
-    font-weight: 600;
-    font-size: ${props=> props.theme.textSizeTextL};
-    color: ${props=> props.theme.textColor};
+  display: flex;
+  width: 100%;
+  flex: 0 0 auto;
+  font-weight: 600;
+  font-size: ${(props) => props.theme.textSizeTextL};
+  color: ${(props) => props.theme.textColor};
 `;
 
 export const Torrents = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-gap: 10px;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
 `;
 
+export const CommentSection = styled.div`
+  margin: 15px;
+  margin-bottom: 24px;
+`;
 
+export const CommentForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
 
+export const UserNameInput = styled.input`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  font-size: ${(props) => props.theme.textSizeTextL700};
+`;
+
+export const CommentTextarea = styled.textarea`
+  margin-bottom: 8px;
+  border-radius: 4px;
+  resize: none;
+`;
+
+export const CommentSubmitButton = styled.button`
+  background-color: #fae0fd;
+  width: auto;
+  color: #000;
+  padding: 8px 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  letter-spacing: 1px;
+  font-weight: 600;
+  font-family: Roboto;
+  font-size: ${(props) => props.theme.textSizeTextM};
+  margin: 0 auto;
+  transition: all 0.3s ease;
+  &:hover {
+    background-color: #fae0fd;
+    scale: 1.05;
+  }
+`;
+
+export const CommentText = styled.ul`
+  font-weight: 500;
+  list-style: none;
+  padding: 0;
+  font-size: ${(props) => props.theme.textSizeTextL700};
+`;
+
+export const CommentItem = styled.div`
+  background-color: ${(props) => props.theme.CardColor};
+  padding: 8px 20px 8px 20px;
+  margin-bottom: 8px;
+  border-radius: 4px;
+`;
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+export const TextArea = styled.textarea`
+  width: 99.5%;
+  border: 1px solid #ccc;
+  margin-bottom: 8px;
+`;
+
+export const UserCommentsText = styled.div`
+  display: flex;
+  font-size: ${(props) => props.theme.textSizeTitle};
+  letter-spacing: 1px;
+  font-weight: 700;
+  width: 100%;
+  justify-content: start;
+  margin: 30px 0px 30px 0px;
+  text-align: start;
+`;
--- src/components/templates/Details/index.tsx
+++ src/components/templates/Details/index.tsx
@@ -11,11 +11,44 @@ import { AiTwotoneLike } from "react-icons/ai";
 
 import Link from "next/link";
 import { BiTimeFive } from "react-icons/bi";
+
+import { useState, useEffect } from "react";
+import { CommentProps } from "./CommentItem/CommentItem";
+import CommentItem from "./CommentItem/CommentItem";
+
+import { FaStar } from "react-icons/fa";
+import { FaRegThumbsUp } from "react-icons/fa";
+import { FaRegClock } from "react-icons/fa";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+
 const Details = () => {
+  var pageKey = "";
+  if (typeof window !== "undefined" && window.localStorage) {
+    pageKey = window.location.href;
+  }
   const router = useRouter();
   const { filmRetrieve, isLoading } = useFilmRetrieve(
     (router.query.id as string) || "",
   );
+
+  const [comments, setComments] = useState<CommentProps[]>(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const pageKey = window.location.href;
+      const localData = localStorage.getItem(pageKey);
+      return localData ? JSON.parse(localData) : [];
+    }
+    return [];
+  });
+
+  const [userName, setUserName] = useState("");
+  const [commentText, setCommentText] = useState("");
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const pageKey = window.location.href;
+      localStorage.setItem(pageKey, JSON.stringify(comments));
+    }
+  }, [comments]);
 
   if (isLoading) {
     return <Loader />;
@@ -35,6 +68,72 @@ const Details = () => {
       />
     );
   });
+
+  function commentsList() {
+    function addComment(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+      if (userName.trim() !== "" && commentText.trim() !== "") {
+        const currentDate = new Date().toLocaleDateString();
+        const newComment: CommentProps = {
+          id: comments.length + 1,
+          userName: userName,
+          commentText: commentText,
+          time: currentDate,
+        };
+        setComments([...comments, newComment]);
+        setUserName("");
+        setCommentText("");
+      } else {
+        alert(
+          "Please enter both your user name and comment before submitting.",
+        );
+      }
+    }
+
+    function handleUserNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+      setUserName(event.target.value);
+    }
+
+    function handleCommentTextChange(
+      event: React.ChangeEvent<HTMLTextAreaElement>,
+    ) {
+      setCommentText(event.target.value);
+    }
+
+    const commentItems = comments.map((comment) => (
+      <CommentItem
+        key={comment.id}
+        id={comment.id}
+        userName={comment.userName}
+        commentText={comment.commentText}
+        time={comment.time}
+        comments={comments}
+        setComments={setComments}
+      />
+    ));
+    return (
+      <Style.CommentSection>
+        <Style.CommentForm onSubmit={addComment}>
+          <Style.UserInfo>
+            <Style.CommentText> USERNAME:</Style.CommentText>
+            <Style.UserNameInput
+              value={userName}
+              onChange={handleUserNameChange}
+            />
+          </Style.UserInfo>
+
+          <Style.TextArea
+            value={commentText}
+            onChange={(e) => handleCommentTextChange(e)}
+          />
+
+          <Style.CommentSubmitButton>SUBMIT COMMENT</Style.CommentSubmitButton>
+        </Style.CommentForm>
+
+        {commentItems}
+      </Style.CommentSection>
+    );
+  }
 
   return (
     <Style.Details>
@@ -100,6 +199,8 @@ const Details = () => {
             <Style.Torrents>{torrentsList}</Style.Torrents>
           </Style.Description>
         </Style.Data>
+        <Style.UserCommentsText>User Comments</Style.UserCommentsText>
+        <Style.CommentItem>{commentsList()}</Style.CommentItem>
       </Style.Content>
     </Style.Details>
   );
